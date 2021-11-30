f6d806e4a268fd3cb2d57e712ec1f9142caac5bd
meguri-miyano <86993875+meguri-miyano@users.noreply.github.com>
2021-11-30 14:39:12 +0900

    Update urls.py

--- menu_register2/menu2/urls.py
+++ menu_register2/menu2/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'menu'
 urlpatterns = [
   path('menu/', views.FormView.as_view(),name='menuindex'),
-  path('menulist/',views.list_index, name='listindex'),
+  path('menulist/',views.MenuListView.as_view(), name='listindex'),
   path('',views.top_index,name='topindex'),
+  #path('menu-detail/<int:pk>/',views.MenuDetailView.as_view(),name='detail'),
 ]
