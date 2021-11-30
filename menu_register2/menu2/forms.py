f3c252abf1f38a87a2f9d14dacd20c56ccac6d0b
meguri-miyano <86993875+meguri-miyano@users.noreply.github.com>
2021-11-30 14:40:21 +0900

    Update forms.py

--- menu_register2/menu2/forms.py
+++ menu_register2/menu2/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import Menudata
+from .import models
 
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = Menudata
-        fields = ('menuname',)
-        labels={
-            'menuname':'メニュー名',
-        }
+        fields = ('menuname','')
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['menuname'].widget.attrs['class'] = 'form-cntrol col-9'
+        self.fields['menuname'].widget.attrs['placeholder'] = 'メニュー名を入力してください。'
+    
+    
+
+
